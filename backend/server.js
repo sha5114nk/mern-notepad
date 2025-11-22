150ff8855ae74e98d83f80935a94694901a2c815
Shashank Kempasagara <sha5114nk@gmail.com>
2025-11-22 17:58:22 +0530

    Add hot reloading for dev & start script for prod

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express()
 
+app.get('/api/notes', (req, res) => {
+    res.send('Example response')
+})
+
 app.listen(5001, () => {
     console.log('Server started on PORT: 5001')
 })
