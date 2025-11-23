5730926dca402d92796d1e6159e0557e0b47dee8
Shashank Kempasagara <sha5114nk@gmail.com>
2025-11-23 23:03:10 +0530

    Add rate limiting

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js'
+import rateLimiter from './middleware/rateLimiter.js'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -25,6 +26,8 @@ app.use((req, res, next) => { //resp time tracker
     })
     next()
 })
+
+app.use(rateLimiter)
 
 app.use('/api/notes', notesRoutes)
 
