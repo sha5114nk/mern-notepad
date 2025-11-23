92491821cdd7234b1e98ae6bc55a8f64ef62ae4a
Shashank Kempasagara <sha5114nk@gmail.com>
2025-11-23 23:30:35 +0530

    Add frontend skeleton

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes, Route } from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import CreatePage from './pages/CreatePage.jsx'
+import NoteDetailPage from './pages/NoteDetailPage.jsx'
+import toast from 'react-hot-toast'
+
+const App = () => {
+  return (
+    <div data-theme="forest" className='min-h-screen bg-base-200'>
+      <button onClick={()=>toast.success('Success')} className='btn btn-outline'>Toast test</button>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+        <Route path='/note/:id' element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
