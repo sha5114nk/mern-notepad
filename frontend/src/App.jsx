284d3bff4191abdd5fd2df2f3b23f633c56a5105
Shashank Kempasagara <sha5114nk@gmail.com>
2025-12-01 07:42:10 +0530

    Frontend: Add homepage with navbar, ratelimit UI, note lists

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
 import { Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage.jsx'
 import CreatePage from './pages/CreatePage.jsx'
 import NoteDetailPage from './pages/NoteDetailPage.jsx'
-import toast from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div data-theme="forest" className='min-h-screen bg-base-200'>
-      <button onClick={()=>toast.success('Success')} className='btn btn-outline'>Toast test</button>
+    <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/create' element={<CreatePage />} />
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'; 
+import toast from 'react-hot-toast'
+ 
+import Navbar from '../components/Navbar.jsx'
+import RateLimitedUI from '../components/RateLimitedUI.jsx';
+import NoteCard from '../components/NoteCard.jsx';
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true );
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get('http://localhost:5001/api/notes');
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.error('Error fetching notes:', error);
+        if (error.response && error.response.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error('Failed to fetch notes. Please try again later.');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();  
+  }, []);
   return (
-    <div>HomePage</div>
-  )
+    <div className='min-h-screen'>
+      <Navbar />
+      {isRateLimited ? <RateLimitedUI /> :
+      //Notes list
+      (<div className="container max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
+        {notes.length && !isRateLimited && (
+          <div className="grid grid-cols -1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} />
+              // <div key={note._id}>
+              //   {note.title} | {note.content}
+              // </div>
+            ))}
+
+          </div>
+        )}
+      </div>)}
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage 
