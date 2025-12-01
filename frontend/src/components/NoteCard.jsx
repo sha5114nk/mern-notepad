a43affc882415eae01e545b79256a67a31164c91
Shashank Kempasagara <sha5114nk@gmail.com>
2025-12-01 14:25:16 +0530

    Frontend: Add delete functionality with NoNoteFound ui

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,9 +1,25 @@
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router";
+import toast from "react-hot-toast";
 
 import { formatDate } from "../lib/utils.js";
+import api from "../lib/axios.js";
 
-const NoteCard = ({note}) => {
+const NoteCard = ({ note, setNotes }) => {
+    const handleDelete = async (e) => {
+        e.preventDefault();
+        console.log("delete clicked", note);
+        if (!window.confirm("Are you sure you want to delete this note?")) return;
+        try {
+            await api.delete(`/notes/${note._id}`);
+            setNotes((prevNotes) => prevNotes.filter(prevNote => prevNote._id !== note._id));
+            toast.success("Note deleted successfully");
+        } catch (error) {
+            console.error("Error deleting note:", error);
+            toast.error("Failed to delete note. Please try again later.");
+        }
+    };
+
     return <Link to={`/note/${note._id}`}  className="card bg-base-100 hover:shadow-lg transition-all duration-200 
       border-t-4 border-solid border-[#00FF9D]">
         <div className="card-body">
@@ -13,7 +29,7 @@ const NoteCard = ({note}) => {
                 <span className="text-sm text-base-content/50">{formatDate(new Date(note.createdAt))}</span>
                 <div className="flex items-center gap-1">
                     <PenSquareIcon className="size-4" />
-                    <button className="btn btn-ghost btn-xs text-error ">
+                    <button className="btn btn-ghost btn-xs text-error" onClick={handleDelete}>
                         <Trash2Icon className="size-4" />
                     </button>
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,55 +1,64 @@
-import { useEffect, useState } from 'react'
-import toast from 'react-hot-toast'
-import api from '../lib/axios.js'; 
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import api from "../lib/axios.js";
 
-import Navbar from '../components/Navbar.jsx'
-import RateLimitedUI from '../components/RateLimitedUI.jsx';
-import NoteCard from '../components/NoteCard.jsx';
+import Navbar from "../components/Navbar.jsx";
+import RateLimitedUI from "../components/RateLimitedUI.jsx";
+import NoteCard from "../components/NoteCard.jsx";
+import NotesNotFound from "../components/NotesNotFound.jsx";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
   const [notes, setNotes] = useState([]);
-  const [loading, setLoading] = useState(true );
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await api.get('/notes');
+        const res = await api.get("/notes");
         console.log(res.data);
         setNotes(res.data);
         setIsRateLimited(false);
       } catch (error) {
-        console.error('Error fetching notes:', error);
+        console.error("Error fetching notes:", error);
         if (error.response && error.response.status === 429) {
           setIsRateLimited(true);
         } else {
-          toast.error('Failed to fetch notes. Please try again later.');
+          toast.error("Failed to fetch notes. Please try again later.");
         }
       } finally {
         setLoading(false);
       }
     };
-    fetchNotes();  
+    fetchNotes();
   }, []);
   return (
-    <div className='min-h-screen'>
+    <div className="min-h-screen">
       <Navbar />
-      {isRateLimited ? <RateLimitedUI /> :
-      //Notes list
-      (<div className="container max-w-7xl mx-auto p-4 mt-6">
-        {loading && (
-          <div className="text-center text-primary py-10">Loading notes...</div>
-        )}
-        {!!notes.length && !isRateLimited && (
-          <div className="grid grid-cols -1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {notes.map((note) => (
-              <NoteCard key={note._id} note={note} />
-            ))}
-          </div>
-        )}
-      </div>)}
+      {isRateLimited ? (
+        <RateLimitedUI />
+      ) : (
+        //Notes list
+        <div className="container max-w-7xl mx-auto p-4 mt-6">
+          {loading && (
+            <div className="text-center text-primary py-10">
+              Loading notes...
+            </div>
+          )}
+
+          {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
+          {!!notes.length && !isRateLimited && (
+            <div className="grid grid-cols -1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {notes.map((note) => (
+                <NoteCard key={note._id} note={note} setNotes={setNotes} />
+              ))}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default HomePage 
+export default HomePage;
