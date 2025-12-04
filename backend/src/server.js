712e143a3aa06feca93c4691ac21c07b7ae91aca
Shashank Kempasagara <sha5114nk@gmail.com>
2025-12-04 21:44:15 +0530

    Ops: Make deployment ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from 'path';
 
 import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js'
@@ -10,6 +11,7 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
 
 //middleware
 app.use(express.json()) /* Tells Express: “For every incoming request, if the body is JSON (e.g. Content-Type: application/json), read it and parse it.”
@@ -19,13 +21,27 @@ Without express.json(), req.body would be undefined for JSON requests unless you
 app.use((req, res, next) => { //resp time tracker
     const start = Date.now()
     res.on('finish', () => {
-        console.log(`${req.method} ${req.originalUrl} | Response time: ${Date.now()-start}ms`)
+      console.log(`${req.method} ${req.originalUrl} | Response time: ${Date.now()-start}ms`)
     })
     next()
 })
-app.use(cors())
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({
+    origin: 'http://localhost:5173'
+  }));
+}
 app.use(rateLimiter)
 app.use('/api/notes', notesRoutes)
+
+if (process.env.NODE_ENV === 'production') {  
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+
+}
 
 const startServer = async () => {
   try {
